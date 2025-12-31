3aa627d2768807e131031a0b2da759427fa3dbc3
bb <bowenblyons@proton.me>
2025-12-31 15:53:29 -0600

    started powershell track

---
+++ powershell/HelloWorld.ps1
@@ -0,0 +1,16 @@
+Function Get-HelloWorld {
+    <#
+    .SYNOPSIS
+    Outputs "Hello, World!"
+    
+    .DESCRIPTION
+    Output "Hello, World!".
+    
+    .EXAMPLE
+    Get-HelloWorld
+    #>	
+    
+    Return "Hello, World!"
+}
+
+Get-HelloWorld
